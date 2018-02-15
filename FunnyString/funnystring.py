fc8964f6da19aec10e33f863a1d413485a4592ab
Mritunjay Prasad <mritunjayprasad91@gmail.com>
2018-02-15 23:33:23 +0530

    modified FunnyString

--- FunnyString/funnystring.py
+++ FunnyString/funnystring.py
@@ -2,11 +2,24 @@
 
 import sys
 
-def funnyString(s):
-    
+def funnyString(s, q):
+    ans = [''] * q
+    for item in range(0, len(s)):
+        count = 0
+        for i in range(0, len(s[item])-1):
+            if abs(ord(s[item][i+1])-ord(s[item][i])) == abs(ord(s[item][len(s)-i-1])-ord(s[item][len(s)-i-2])):
+                count += 1
+        if count == len(s[item])-1:
+            ans[item] = 'Funny'
+        else:
+            ans[item] = 'Not Funny'
+    return ans
 
 q = int(input().strip())
+s = ['NO'] * q
 for a0 in range(q):
-    s = input().strip()
-    result = funnyString(s)
-    print(result)
+    aa = input().strip()
+    s[a0] = aa
+result = funnyString(s, q)
+for i in range(0, len(s)):
+    print(result[i])
